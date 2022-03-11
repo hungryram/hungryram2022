7b3963e9c63b3d152a03bdfdba621f0234a5f9e0
Ram Dettmer <ramdettmer751@gmail.com>
2022-03-10 17:59:30 -0800

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { BsChevronDown } from "@react-icons/all-files/bs/BsChevronDown";
+import { BsChevronRight } from "@react-icons/all-files/bs/BsChevronRight";
 import * as Styles from "../styles/navbar.module.css"
 import Logo from "../images/2021-hungry-ram-logo-black.png"
 
@@ -25,7 +26,7 @@ export default function Navbar() {
                             </ul>
                         </div>
                         <div className="uk-navbar-right">
-                        <ul className="uk-navbar-nav">
+                            <ul className="uk-navbar-nav">
                                 <li>
                                     <a href="tel:(657) 549-5082)" target="_blank" rel="noopener">(657) 549-5082)</a>
                                 </li>
@@ -94,7 +95,7 @@ export default function Navbar() {
                                     </li>
                                     <li><Link to="/contact/">Contact</Link></li>
                                     <div className="uk-navbar-item">
-                                        <li><a class="uk-button uk-button-primary" href="https://calendly.com/hungry-ram" rel="noopener" target="_blank">Book a call</a></li>
+                                        <li><a className="uk-button uk-button-primary" href="https://calendly.com/hungry-ram" rel="noopener" target="_blank">Book a call</a></li>
                                     </div>
                                 </ul>
                             </div>
@@ -106,60 +107,98 @@ export default function Navbar() {
 
 
 
-
-
             <div data-uk-sticky="animation: uk-animation-slide-top-medium; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; cls-inactive: uk-dark; cls-inactive:uk-navbar-transparent; top: 400">
                 <nav className="uk-navbar uk-container uk-hidden@m uk-box-shadow-small">
                     <div className="uk-navbar-left">
                         <a href="/">
-                            <img src="https://res.cloudinary.com/hungryram19/image/upload/v1631928088/hungryram/2021-hungry-ram-logo-black_hsw94p.png" alt="Hungry Ram Web Design" width="200" class="dark-logo" />
+                            <img src="https://res.cloudinary.com/hungryram19/image/upload/v1631928088/hungryram/2021-hungry-ram-logo-black_hsw94p.png" alt="Hungry Ram Web Design" width="200" className="dark-logo" />
                         </a>
                     </div>
                     <div className="uk-navbar-right">
-                        <a className="uk-navbar-toggle" data-uk-navbar-toggle-icon data-uk-toggle="target: #mobile-menu"></a>
+                        <a className="uk-navbar-toggle" data-uk-navbar-toggle-icon data-uk-toggle="target: #modal-full"></a>
                     </div>
                 </nav>
-
+                {/* 
                 <div id="mobile-menu" uk-offcanvas="mode: reveal">
-                    <div className="uk-offcanvas-bar uk-flex uk-flex-column">
-                        <ul className="uk-nav-default uk-nav-parent-icon" data-uk-nav>
-                            <li className="uk-nav-header">Hungry Ram</li>
-
-                            <li className="uk-nav-header"></li>
-                            <li><Link to="">Home</Link></li>
+                    <div className={`uk-offcanvas-bar uk-flex uk-flex-column ${Styles.ukOffcanvasBar}`}>
+                        <ul className="uk-nav uk-margin-auto-vertical uk-dark" data-uk-nav>
+                            <li><Link to="/">Home</Link></li>
                             <li className="uk-parent">
-                                <a to="#">Services</a>
+                                <Link to="#">About <BsChevronRight /></Link>
                                 <ul className="uk-nav-sub">
-                                    <li className="uk-nav-header">Real Estate Websites</li>
-                                    <li className="uk-nav-divider"></li>
-                                    <li><Link to="/services/real-estate-agent-websites/">Agent websites</Link></li>
-                                    <li><Link to="/services/real-estate-broker-websites/">Broker websites</Link></li>
-                                    <li className="uk-nav-header">IDX Websites</li>
-                                    <li className="uk-nav-divider"></li>
-                                    <li><Link to="/services/agent-idx-websites/">Agent IDX</Link></li>
-                                    <li><Link to="/services/broker-idx-websites/">Broker IDX</Link></li>
-                                    <li className="uk-nav-header">Small Business Websites</li>
-                                    <li className="uk-nav-divider"></li>
-                                    <li><Link to="/services/ecommerce-website-design/">Ecommerce website</Link></li>
-                                    <li><Link to="/services/small-business-website-design/">Small business website</Link></li>
+                                    <li><Link to="/about/">About</Link></li>
+                                    <li><Link to="/portfolio/">Portfolio</Link></li>
                                 </ul>
                             </li>
                             <li className="uk-parent">
-                                <a to="#">About</a>
+                                <Link to="#">Services <BsChevronRight /></Link>
                                 <ul className="uk-nav-sub">
-                                    <li className="uk-nav-header">Hungry Ram</li>
-                                    <li className="uk-nav-divider"></li>
-                                    <li><Link to="/about/">About</Link></li>
-                                    <li><Link to="/portfolio/">Portfolio</Link></li>
-                                    <li><Link to="/contact/">Contact</Link></li>
-                                    <li className="uk-nav-header">Resources</li>
-                                    <li className="uk-nav-divider"></li>
-                                    <li><Link to="/faq/">FAQ</Link></li>
+                                    <li><Link to="/services/">All Services</Link></li>
+                                    <li><Link to="/services/agent-idx-websites/">Agent IDX</Link></li>
+                                    <li><Link to="/services/broker-idx-websites/">Broker IDX</Link></li>
+                                    <li><Link to="/services/real-estate-agent-websites/">Agent websites</Link></li>
+                                    <li><Link to="/services/real-estate-broker-websites/">Broker websites</Link></li>
+                                    <li><Link to="/services/small-business-website-design/">Small business website</Link></li>
+                                    <li><Link to="/services/ecommerce-website-design/">Ecommerce website</Link></li>
+                                </ul>
+                            </li>
+                            <li className="uk-parent">
+                                <Link to="#">Resources <BsChevronRight /></Link>
+                                <ul className="uk-nav-sub">
                                     <li><Link to="/blog/">Blog</Link></li>
+                                    <li><Link to="/faq/">FAQ</Link></li>
                                 </ul>
                             </li>
                             <li><Link to="/contact">Contact</Link></li>
                         </ul>
+                    </div>
+                </div> */}
+
+                <div id="modal-full" className="uk-modal-full" data-uk-modal>
+                    <div className={`uk-modal-dialog ${Styles.ukOffcanvasBar}`} data-uk-height-viewport>
+                        <button className="uk-modal-close-full uk-close-large" type="button" data-uk-close></button>
+                        <div>
+                            <div className="uk-padding-large">
+                                <div className="uk-child-width-1-2@s" data-uk-grid>
+                                    <div className="uk-margin-large">
+                                        <img src="https://res.cloudinary.com/hungryram19/image/upload/v1631928088/hungryram/2021-hungry-ram-logo-black_hsw94p.png" alt="Hungry Ram Web Design" width="200" />
+                                    </div>
+                                </div>
+                                <ul className="uk-nav uk-margin-auto-vertical uk-dark" data-uk-nav>
+                                    <li className={`uk-parent ${Styles.anchor}`}><Link to="/">Home</Link></li>
+                                    <li className={`uk-parent ${Styles.anchor}`}>
+                                        <Link to="#">About <BsChevronRight /></Link>
+                                        <ul className="uk-nav-sub">
+                                            <li><Link to="/about/">About</Link></li>
+                                            <li><Link to="/portfolio/">Portfolio</Link></li>
+                                        </ul>
+                                    </li>
+                                    <li className={`uk-parent ${Styles.anchor}`}>
+                                        <Link to="#">Services <BsChevronRight /></Link>
+                                        <ul className="uk-nav-sub">
+                                            <li><Link to="/services/">All Services</Link></li>
+                                            <li><Link to="/services/agent-idx-websites/">Agent IDX</Link></li>
+                                            <li><Link to="/services/broker-idx-websites/">Broker IDX</Link></li>
+                                            <li><Link to="/services/real-estate-agent-websites/">Agent websites</Link></li>
+                                            <li><Link to="/services/real-estate-broker-websites/">Broker websites</Link></li>
+                                            <li><Link to="/services/small-business-website-design/">Small business website</Link></li>
+                                            <li><Link to="/services/ecommerce-website-design/">Ecommerce website</Link></li>
+                                        </ul>
+                                    </li>
+                                    <li className={`uk-parent ${Styles.anchor}`}>
+                                        <Link to="#">Resources <BsChevronRight /></Link>
+                                        <ul className="uk-nav-sub">
+                                            <li><Link to="/blog/">Blog</Link></li>
+                                            <li><Link to="/faq/">FAQ</Link></li>
+                                        </ul>
+                                    </li>
+                                    <li className={`uk-parent ${Styles.anchor}`}><Link to="/contact">Contact</Link></li>
+                                </ul>
+                                <div>
+                                <Link to="/contact" className={`uk-button uk-button-primary uk-width-1-1 ${Styles.mobileCta}`}>Get Started</Link>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
